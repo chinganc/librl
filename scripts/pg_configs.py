1f9fadc886a865a66d193dd6bc36389b26166543
Ching-An Cheng <cacheng@gatech.edu>
2019-07-29 23:40:51 -0400

    fix a typo in pg_configs

--- scripts/pg_configs.py
+++ scripts/pg_configs.py
@@ -4,7 +4,7 @@ from scripts import configs as dc
 from rl.core.utils.misc_utils import dict_update
 
 config_hopper = copy.deepcopy(CONFIG)
-config_hopper = dict_update(config_hp, dc.config_hopper)
+config_hopper = dict_update(config_hopper, dc.config_hopper)
 config_hopper['algorithm']['lr']=0.01
 config_hopper['policy_units']=(64,64)
 
