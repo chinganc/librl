2483fcfc8100f43a2e0027b9ee13406efce974b9
Ching-An Cheng <cacheng@gatech.edu>
2019-08-01 21:59:58 -0400

    make plot callable; remove gpg_new

--- utils/plot.py
+++ utils/plot.py
@@ -42,7 +42,69 @@ def read_attr(csv_path, attr):
     return np.array(vals, dtype=np.float64)
 
 
-def main():
+def main(logdir, value, output_dir=None, filename=None, style=None,
+         y_higher=None, y_lower=None, n_iters=None, legend_loc=0,
+         curve_style='percentile'):
+
+    attr = value
+    conf = Configs(style)
+    subdirs = sorted(os.listdir(logdir))
+    subdirs = [d for d in subdirs if d[0] != '.']  # filter out weird things, e.g. .DS_Store
+    subdirs = conf.sort_dirs(subdirs)
+    fontsize = 32 if style else 12  # for non style plots, exp name can be quite long
+    usetex = True if style else False
+    configure_plot(fontsize=fontsize, usetex=usetex)
+    linewidth = 4
+    n_curves = 0
+    for exp_name in subdirs:
+        exp_dir = os.path.join(logdir, exp_name)
+        if not os.path.isdir(exp_dir):
+            continue
+        data = []
+        for root, _, files in os.walk(exp_dir):
+            if 'log.txt' in files:
+                d = read_attr(os.path.join(root, 'log.txt'), value)
+                if d is not None:
+                    data.append(d)
+        if not data:
+            continue
+        n_curves += 1
+        data = np.array(truncate_to_same_len(data))
+        if curve_style == 'std':
+            mean, std = np.mean(data, axis=0), np.std(data, axis=0)
+            low, mid, high = mean - std, mean, mean + std
+        elif curve_style == 'percentile':
+            low, mid, high = np.percentile(data, [25, 50, 75], axis=0)
+        if n_iters is not None:
+            mid, high, low = mid[:n_iters], high[:n_iters], low[:n_iters]
+        n_iters = mid.size
+        iters = np.arange(n_iters)
+        if not exp_name:
+            continue
+        color = conf.color(exp_name)
+        plt.plot(iters, mid, label=conf.label(exp_name), color=color, linewidth=linewidth)
+        plt.fill_between(iters, low, high, alpha=0.25, facecolor=color)
+    if n_curves == 0:
+        print('Nothing to plot.')
+        return 0
+    if not style:
+        plt.xlabel('Iteration', fontsize=fontsize)
+        plt.ylabel(value, fontsize=fontsize)
+    legend = plt.legend(loc=legend_loc, fontsize=fontsize, frameon=False)
+    plt.autoscale(enable=True, tight=True)
+    plt.tight_layout()
+    plt.ylim(y_lower, y_higher)
+    plt.grid(linestyle='--', linewidth='0.2')
+    for line in legend.get_lines():
+        line.set_linewidth(6.0)
+    output_dir = output_dir or logdir
+    output_filename = filename or '{}.pdf'.format(value)
+    output_path = os.path.join(output_dir, output_filename)
+    plt.savefig(output_path)
+    plt.clf()
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-d','--dir', help='The dir of experiments', type=str)
     parser.add_argument('-v', '--value', help='The column name in the log.txt file', type=str)
@@ -56,64 +118,14 @@ def main():
     parser.add_argument('--curve', type=str, default='percentile', help='percentile, std')
 
     args = parser.parse_args()
-    args.attr = args.value
-    args.dir = args.dir
-    conf = Configs(args.style)
-    subdirs = sorted(os.listdir(args.dir))
-    subdirs = [d for d in subdirs if d[0] != '.']  # filter out weird things, e.g. .DS_Store
-    subdirs = conf.sort_dirs(subdirs)
-    fontsize = 32 if args.style else 12  # for non style plots, exp name can be quite long
-    usetex = True if args.style else False
-    configure_plot(fontsize=fontsize, usetex=usetex)
-    linewidth = 4
-    n_curves = 0
-    for exp_name in subdirs:
-        exp_dir = os.path.join(args.dir, exp_name)
-        if not os.path.isdir(exp_dir):
-            continue
-        data = []
-        for root, _, files in os.walk(exp_dir):
-            if 'log.txt' in files:
-                d = read_attr(os.path.join(root, 'log.txt'), args.attr)
-                if d is not None:
-                    data.append(d)
-        if not data:
-            continue
-        n_curves += 1
-        data = np.array(truncate_to_same_len(data))
-        if args.curve == 'std':
-            mean, std = np.mean(data, axis=0), np.std(data, axis=0)
-            low, mid, high = mean - std, mean, mean + std
-        elif args.curve == 'percentile':
-            low, mid, high = np.percentile(data, [25, 50, 75], axis=0)
-        if args.n_iters is not None:
-            mid, high, low = mid[:args.n_iters], high[:args.n_iters], low[:args.n_iters]
-        n_iters = mid.size
-        iters = np.arange(n_iters)
-        if not exp_name:
-            continue
-        color = conf.color(exp_name)
-        plt.plot(iters, mid, label=conf.label(exp_name), color=color, linewidth=linewidth)
-        plt.fill_between(iters, low, high, alpha=0.25, facecolor=color)
-    if n_curves == 0:
-        print('Nothing to plot.')
-        return 0
-    if not args.style:
-        plt.xlabel('Iteration', fontsize=fontsize)
-        plt.ylabel(args.attr, fontsize=fontsize)
-    legend = plt.legend(loc=args.legend_loc, fontsize=fontsize, frameon=False)
-    plt.autoscale(enable=True, tight=True)
-    plt.tight_layout()
-    plt.ylim(args.y_lower, args.y_higher)
-    plt.grid(linestyle='--', linewidth='0.2')
-    for line in legend.get_lines():
-        line.set_linewidth(6.0)
-    output_dir = args.output_dir or args.dir
-    output_filename = args.filename or '{}.pdf'.format(args.attr)
-    output_path = os.path.join(output_dir, output_filename)
-    plt.savefig(output_path)
-    plt.clf()
 
+    main(logdir=args.dir,
+         value=args.value,
+         output_dir=args.output_dir,
+         filename=args.filename,
+         style=args.style,
+         y_higher=args.y_higher,
+         y_lower=args.y_lower,
+         legend_loc=args.legend_loc,
+         curve_style=args.curve)
 
-if __name__ == "__main__":
-    main()
