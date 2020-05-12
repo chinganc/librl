Correct trajectory-level importance sampling and move the expert value update before the gradient computation
1. Remove the horizon factor in the importance sampling of the swtiching time.
2. Use geometric distribution instead of exponential distribution in sampling the switching time (the latter is removed).
3. Simplify the codes of generating the switching time in RIRO.
4. Account for the importance sampling due to using epsilon-greedy.
5. Move the expert value update before the gradient computation.
6. Fix typos in comments.

rl/adv_estimators/advantage_estimator.py
@@ -106,7 +106,7 @@ class ValueBasedAE(AdvantageEstimator):
         self.pe_lambd = pe_lambd  # user-defined self.pe_lambda-weighted td error
         if np.isclose(self.pe_lambd, 1.0):
             n_pe_updates = 1
-        assert n_pe_updates >= 1, 'Policy evaluation needs at least one udpate.'
+        assert n_pe_updates >= 1, 'Policy evaluation needs at least one update.'
         self._n_pe_updates = n_pe_updates
         assert isinstance(vfn, SupervisedLearner)
         self.vfn = vfn