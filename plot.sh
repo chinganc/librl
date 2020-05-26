736eb96fd835d645931e9da1b8121685598cf918
chinganc <hamdonas@gmail.com>
2020-05-26 13:32:50 -0700

    add init_learner for DartDoubleInvertedPendulumEnv-v1

--- plot.sh
+++ plot.sh
@@ -3,3 +3,9 @@ plot_folder=$2
 python utils/plot.py -v MeanSumOfRewards -d $log_folder -o $plot_folder &
 python utils/plot.py -v BestSumOfRewards -d $log_folder -o $plot_folder &
 python utils/plot.py -v std -d $log_folder -o $plot_folder &
+
+
+python utils/plot.py -v "ExplainVarianceBefore(AE)" -d $log_folder -o $plot_folder &
+python utils/plot.py -v "ExplainVarianceAfter(AE)"  -d $log_folder -o $plot_folder &
+python utils/plot.py -v "MeanExplainVarianceAfter(AE)"  -d $log_folder -o $plot_folder &
+python utils/plot.py -v "MeanExplainVarianceBefore(AE)"  -d $log_folder -o $plot_folder &
