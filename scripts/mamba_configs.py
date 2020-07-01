bdaa2daf014c30eb09c04df347d43e02e59dc4cf
chinganc <hamdonas@gmail.com>
2020-07-01 00:33:43 -0700

    add expert training configs

--- scripts/mamba_configs.py
+++ scripts/mamba_configs.py
@@ -14,3 +14,8 @@ config_humanoid = copy.deepcopy(CONFIG)
 config_humanoid = dict_update(config_humanoid, dc.config_humanoid_traj)
 config_humanoid['experimenter']['ro_kwargs']['max_n_rollouts']=50
 
+
+config_humanoid_expert = copy.deepcopy(CONFIG)
+config_humanoid_expert = dict_update(config_humanoid_expert, dc.config_humanoid)
+config_humanoid_expert['experimenter']['n_itrs']=3000
+
