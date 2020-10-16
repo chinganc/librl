06c430dec4811817e8f20b9d6be36c500ff25ee9
chinganc <chinganc@microsoft.com>
2020-10-16 16:12:41 -0700

    update the debugging range

--- scripts/mamba_ranges.py
+++ scripts/mamba_ranges.py
@@ -105,6 +105,7 @@ range_agg_debug= [
     [['algorithm', 'lr'], [0.1, 0.05, 0.001]],
     [['algorithm', 'n_bc_steps'], [0, 100, 1000]],
     [['algorithm', 'n_value_steps'], [10, 100, 1000]],
+    [['algorithm', 'max_n_batches_experts'], [2, 10, 100,]],
     [['value_units', (256, 256), (512, 512), (256,256,256)]],
 ]
 range_agg_debug = R.merge_ranges(range_common, range_agg_debug)
