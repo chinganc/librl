d08ed4d5192396ed7739a52ee6b8d51c27419ec2
chinganc <chinganc@microsoft.com>
2020-10-20 08:01:39 -0700

    add option to mix experts

--- scripts/mamba.py
+++ scripts/mamba.py
@@ -26,7 +26,7 @@ def load_vfn(path, name):
     return vfn
 
 
-def create_experts(envid, name, path=None, order=True):
+def create_experts(envid, name, path=None, order=True, mix_experts=False, mix_swtich=None):
     def load_expert(path, name):
         expert = load_policy(path, name)
         expert.name = 'expert_policy'
@@ -38,38 +38,81 @@ def create_experts(envid, name, path=None, order=True):
     dirs = os.listdir(path)
     dirs.sort()
 
-    for d in dirs:  # for a single expert
-        if d==name:
-            experts = [load_expert(path, name)]
-            break
-    else: # for a set of experts
-        expert_and_vals = []
-        for d in dirs:
-            d_path = os.path.join(path,d)
-            # load expert
-            expert = load_expert(os.path.join(d_path, 'saved_policies'), name)
-            # load its MeanSumOfRewards
-            score = read_attr(os.path.join(d_path, 'log.txt'), 'MeanSumOfRewards')
-            if name=='policy_best':
-                val = np.max(score)
-            else:
-                itr = int(name.split('_')[-1])
-                val= score[itr]
 
-            expert_and_vals.append([expert, val, d])
+    # Parse expert names
+    if type(name) is str:
+        # Load all the policies with the same name from all the seeds
+        expert_dir_name = [(d,name) for d in dirs]
+    elif type(name) is list:
+        # The seed and policy pair is provided explicitly
+        expert_dir_name = [ string.split('#') for string in name]
+    else:
+        ValueError('Unknown expert names')
 
-        # sort from the best to the worst
-        expert_and_vals.sort(reverse=True, key=lambda i:i[1])
-        experts = [expert for expert, val, d in expert_and_vals]
+    expert_and_vals = []
+    for d, policy_name in expert_dir_name:
+        d_path = os.path.join(path,d)
+        # load expert
+        expert = load_expert(os.path.join(d_path, 'saved_policies'), policy_name)
+        # load its MeanSumOfRewards
+        score = read_attr(os.path.join(d_path, 'log.txt'), 'MeanSumOfRewards')
+        if policy_name=='policy_best':
+            val = np.max(score)
+        else:
+            itr = int(policy_name.split('_')[-1])
+            val= score[itr]
+        expert_and_vals.append([expert, val, d])
 
-        if not order:  # use a random order
-            ind = np.random.permutation(len(experts))
-            experts = [ experts[i] for i in ind]
+    # sort from the best to the worst
+    expert_and_vals.sort(reverse=True, key=lambda i:i[1])
+    experts = [expert for expert, val, d in expert_and_vals]
 
-        for i, expert in enumerate(experts):
-            expert.name = 'expert_'+str(i)
+    if not order:  # use a random order
+        ind = np.random.permutation(len(experts))
+        experts = [ experts[i] for i in ind]
 
+    for i, expert in enumerate(experts):
+        expert.name = 'expert_'+str(i)
+
+    # mix experts into swiching experts
+    if mix_experts:
+        assert mix_swtich is not None
+        new_experts = []
+        for i in range(len(experts)):
+            new_experts.append(SwtichingExpert(experts[i], experts[-1-i], mix_swtich))
+        experts = new_experts
     return experts
+
+
+from rl.core.function_approximators.policies import Policy
+class SwtichingExpert(Policy):
+    def __init__(self, policy_1, policy_2, mix_swtich):
+        self._policy_1 = policy_1
+        self._policy_2 = policy_2
+        self._mix_swtich = mix_swtich
+        self.x_shape = self._policy_1.x_shape
+        self.y_shape = self._policy_1.y_shape
+
+    def predict(self, xs, **kwargs):
+        assert len(xs)==1
+        return np.array([self._call(xs[0])])
+
+    def logp(self, xs, ys): # HACK
+        return np.zeros(len(xs))
+
+    def _call(self, x_and_t):
+        # assume the last entry is time
+        t = x_and_t[-1]
+        if t <= self._mix_swtich:
+            return self._policy_1(x_and_t)
+        else:
+            return self._policy_2(x_and_t)
+
+    @property
+    def variable(self):
+        return None
+
+
 
 def create_learner(envid, seed, policy0, vfn0):
     # Try to load the initial policy and value function
@@ -151,10 +194,10 @@ CONFIG = {
     'exp_name': 'cp',
     'seed': 0,
     'mdp': {
-        'envid':  'DartCartPole-v1',
+        'envid':  'HalfCheetah-v2',
         'horizon': 1000,  # the max length of rollouts in training
         'gamma': 1.0,
-        'n_processes': 8,
+        'n_processes': 4,
         'min_ro_per_process': 2,  # needs to be at least 2 so the experts will be rollout
         'max_run_calls':25,
         'rw_scale':0.01,
@@ -187,8 +230,8 @@ CONFIG = {
         'policy_as_expert': False,
         'max_n_batches_experts':2,
         'use_bc': True,
-        'n_bc_steps':1000*2,
-        'n_value_steps':800,
+        'n_bc_steps':1000,
+        'n_value_steps':200,
     },
     'policy_units': (128,128),
     'value_units': (256,256),
@@ -196,9 +239,11 @@ CONFIG = {
     'init_lstd': -1,
     'use_experts': True,
     'expert_info':{
-        'name':'policy_200',
+        'name': 'policy_200', #['100#policy_10', '500#policy_best'],
         'path': None, #'experts/DartCartPole-v1/'+str(100)+'/saved_policies',
         'order': True,  # True to use the ordering based on performance; False to use a random ordering
+        'mix_experts': True,
+        'mix_swtich': 0.5,
     },
     'n_experts': 2, # None,
 }
